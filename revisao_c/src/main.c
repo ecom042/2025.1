4bff0d0ec4a4bae35ba536acb5ac76165cfdc6dd
Rodrigo Peixoto <rodrigopex@gmail.com>
2025-07-29 16:43:46 -0300

    Conditional compilation
    Signed-off-by: Rodrigo Peixoto <rodrigopex@gmail.com>

--- revisao_c/src/main.c
+++ revisao_c/src/main.c
@@ -1,211 +1,79 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <string.h>
-// #include <zephyr/kernel.h>
-//  Tipos primitivos
+#include <stdlib.h>
 
-// float f;  // i32
-// double d; // i64
+char *os_string = NULL;
+int os_int = 0;
 
-// int i; // i32
+#define CHECK_ARGUMENTS 0
 
-// uint64_t x = 10;
-// int8_t v = 200; // [-128,127]
-// bool b = true;
+/* clang-format off */
+#define MY_DIV(x, y, res)  \
+	do {                   \
+		if (y == 0) {      \
+			*res = -1;     \
+		} else {           \
+			*res = x / y;  \
+		}                  \
+	} while (0)
+/* clang-format on */
 
-// char c = '?'; // (0x3F);
-// char name[] = "John Doe";
-
-// Operações bitwise
-//                                             end
-//                                              |
-int32_t arr[4] = {5, 4, 3, 2}; //{}
-int32_t arr2[] = {true, false, false, true};
-bool arr3[3][3] = {{true, false, false}, {true, false, false}, {true, false, false}};
-int arr4[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-// string de C
-char name[] = "Fulano de Tal";
-char name2[] = {'F', 'u', 'l', 'a', 'n', 'o', ' ', 'd', 'e', ' ', 'T', 'a', 'l', '\0'};
-
-// Array de bytes
-char name3[] = {'F', 'u', 'l', 'a', 'n', 'o', ' ', 'd', 'e', ' ', 'T', 'a', 'l', ' ', ' '};
-char name4[] = "agora foi";
-//   Array
-//   Ponteiros
-uint8_t bytes[100];
-
-// bitfield
-struct config {
-	uint16_t res1: 3;
-	uint16_t acc_en: 1;
-	uint16_t res2: 1;
-	uint16_t led_en: 1;
-	uint16_t res3: 16;
-};
-
-struct payload {
-	uint16_t level: 4;
-	uint16_t temp: 5;             /* 18 + temp */
-	uint16_t last_fill_hour: 5;   /* 24 hours  */
-	uint16_t last_fill_minute: 2; /* 0, 15, 30, 45 minutes */
-};
-
-union payload_data {
-	struct payload payload;
-	uint8_t bytes[2];
-} p;
-
-union variant {
-	char c;
-	short s;
-	int i;
-	bool b;
-	float f;
-};
-
-struct calc {
-	enum {
-		ADD,
-		SUB,
-		DIV,
-		MULT,
-		MINUS
-	} __attribute__((packed)) op;
-	bool flag1;
-	bool flag2;
-	bool flag3;
-	union {
-		struct {
-			int a;
-			int b;
-		};
-		struct {
-			int u;
-		};
-	};
-} __attribute__((aligned(16)));
-
-int op(struct calc *c)
+/**
+ * Divides two integers and stores the result in a pointer.
+ *
+ * @param a The dividend.
+ * @param b The divisor.
+ * @param[out] res Pointer to store the result.
+ * @return 0 on success, -1 on division by zero.
+ */
+int division(int a, int b, int *res)
 {
-	switch (c->op) {
-	case ADD:
-		return c->a + c->b;
-	case SUB:
-		return c->a - c->b;
-	case DIV:
-		return c->a / c->b;
-	case MULT:
-		return c->a * c->b;
-	case MINUS:
-		return -c->u;
+#if defined(CHECK_DIVISION)
+	if (b == 0) {
+		return -1;
 	}
-}
-
-typedef union variant variant_t;
-
-union variant var = {.s = 200};
-// Estruturas
-// Union
-#define BIT(n) (1 << n)
-
-#define LED_ENABLED BIT(5)
-#define ACC_ENABLED BIT(3)
-
-struct point {
-	int x;  // 0 // 4
-	char c; // 2 // 1
-	short s;
-	short s2;
-	// padding 2
-	char c2;
-	int y; // 1 // 4
-	// padding 7
-	int64_t f; // 8
-} __attribute__((packed));
-
-struct point point1 = {10, 20};
-struct point point2 = {30, 40};
-
-int get_arr4_element(int row, int col)
-{
-	int *p4 = (int *)arr4;
-	return p4[row * 3 + col];
+#endif
+	*res = a / b;
+	return 0;
 }
 
 int main(int argc, char *argv[])
 {
-	int32_t *temp = arr;
-	for (int i = 0; i < 4; ++i) {
-		printf("arr[%d] = %d\n", i, temp[i]);
+#if defined(CONFIG_LINUX)
+	os_string = "Linux";
+#elif defined(CONFIG_WINDOWS)
+	os_string = "Windows";
+#else
+	os_string = "Unknown";
+#endif
+
+	printf("Hello, %s (%d)!\n", os_string, os_int);
+
+	if (argc != 3) {
+		printf("Usage: %s <numerator> <denominator>\n", argv[0]);
+		return 1;
 	}
 
-	int32_t *iter = arr;
-	int32_t *end = arr + 4;
-	for (; iter < end; ++iter) {
-		printf("arr[%d] = %d\n", (int)(iter - arr), *iter);
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
+
+#if defined(CHECK_ARGUMENTS)
+	if (a == 0 || b == 0) {
+		printf("Error: Invalid input\n");
+		return 1;
 	}
+#endif
 
-	// TODO: navegar pelo array usando aritimética de ponteiros
+	int result;
+	MY_DIV(a, b, &result);
+	printf("Result of division: %d\n", result);
+	// int err = MY_DIV(a, b, &result);
+	// if (err) {
+	// 	printf("Error: Division by zero\n");
+	// } else {
+	// 	printf("Result of division: %d\n", result);
+	// }
 
-	printf("name size: %zu\n", strlen(name));
-	printf("name size: %zu\n", strlen(name2));
-	printf("name size: %zu\n", strlen(name3));
-
-	int a = 0x0F; // 0000.0001 >> 1 = 0000.0000
-	int b = 0xF0; // !(0000.0000) = 1
-		      // ~0000.0000
-
-	uint8_t config = ACC_ENABLED; // 0010.1000
-
-	if (config & LED_ENABLED) {
-		//
-	} else {
-		config = config | LED_ENABLED;
-	}
-
-	config = config & (~LED_ENABLED);
-
-	int c = a || b; // +
-	int d = a && b; // *
-
-	printf("a = 0x%02X\n", a);
-	printf("b = 0x%02X\n", b);
-	printf("c = 0x%02X\n", c);
-	printf("a & b = 0x%02X\n", a & b);
-	printf("a | b = %d\n", a | b);
-	printf("a ^ b = %d\n", a ^ b);
-	printf("~a = %d\n", ~a);
-	printf("~b = %d\n", ~b);
-	printf("a << 2 = %d\n", a << 2);
-	printf("a >> 2 = %d\n", a >> 2);
-	printf("b << 2 = %d\n", b << 2);
-	printf("b >> 2 = %d\n", b >> 2);
-
-	int x = 10;
-	int y = 100;
-	int *p = &x;
-	*p = 15;
-	// p = &y;
-	*(p - 1) = 25;
-
-	printf("x = %d, &x = %p, y = %d, &y = %p, p = %p, &y - &x = %d\n", x, &x, y, &y, p,
-	       (int)(&x - &y));
-
-	// struct row row[] = {{.col0 = 1, .col1 = 2, .col2 = 3},
-	// 		    {.col0 = 4, .col1 = 5, .col2 = 6},
-	// 		    {.col0 = 7, .col1 = 8, .col2 = 9}};
-
-	// row[0].col0 = 10;
-	// row[0].col1 = 20;
-	// row[0].col2 = 30;
-	printf("value %d\n", get_arr4_element(1, 1));
-
-	printf("sizeof struct point %zu\n", sizeof(struct point));
-
-	uint8_t c1 = ACC_ENABLED | LED_ENABLED;
-	struct config cfg = {.acc_en = 1, .led_en = 0};
-
-	printf("Config %#.02x\n", *((uint8_t *)&cfg));
 	return 0;
 }
